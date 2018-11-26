6e8a1936687adc9f393616c2a3c7a5d22239f247
Alex Faria <afaria@student.uc.pt>
2018-11-26 22:47:35 +0000

    format

--- bdcringe/database/groups.py
+++ bdcringe/database/groups.py
@@ -3,7 +3,7 @@ from bdcringe.database import Database
 
 
 def exists_group(group_name):
-    sql = """SELECT * FROM grupo_musical WHERE nome like %s"""
+    sql = "SELECT * FROM grupo_musical WHERE nome like %s"
     try:
         conn = Database.connect()
         cur = conn.cursor()
@@ -15,7 +15,7 @@ def exists_group(group_name):
 
 
 def insert_new(name, date_begin, date_end):
-    sql = """INSERT INTO grupo_musical(nome, inicio, fim) VALUES(%s, %s, %s)"""
+    sql = "INSERT INTO grupo_musical(nome, inicio, fim) VALUES(%s, %s, %s)"
     try:
         conn = Database.connect()
         cur = conn.cursor()
@@ -28,18 +28,28 @@ def insert_new(name, date_begin, date_end):
 
 
 def add_artist(artist_name, group_name):
-    sql = """INSERT INTO artista_grupo_musical(artista_id, grupo_musical_nome) 
-            SELECT a.id, g.nome 
-            FROM artista a, grupo_musical g 
-            WHERE a.nome like %s and g.nome like %s
-        """
+    sql = """
+    INSERT
+    INTO
+        artista_grupo_musical
+        (artista_id, grupo_musical_nome)
+        SELECT
+            a.id,
+            g.nome 
+        FROM
+            artista a,
+            grupo_musical g
+        WHERE
+            a.nome like %s and
+            g.nome like %s
+    """
     try:
         conn = Database.connect()
         cur = conn.cursor()
         cur.execute(sql, (artist_name, group_name))
         conn.commit()
     except DatabaseError as error:
-        raise DatabaseError(error)
-    else:
-        return 'Success'
+        print(error)
+        return False
 
+    return True
